5dab3b0106d42f886d3a71bb1d39f91eaff6a122
SOROoi <545772909@qq.com>
2020-07-16 23:41:56 +0800

    7月16添加Vue笔记

--- Web01_Vue/src/main/java/vue/Vue详解.java
+++ Web01_Vue/src/main/java/vue/Vue详解.java
@@ -115,39 +115,66 @@ package vue;
 				
 			例:		<script>
 						new Vue({
-							el : "#app",
-							data : {
-								name : "Vue啊",
-								num : 1,
-								message : "你们好！",
-								word : "<h1>世界和平</h1>",
-								dinner : [],
-								peoples : [ {
-									name : '小明',
-									gender : '男',
-									age : 20
-								}, {
-									name : '小红',
-									gender : '女',
-									age : 18
-								}, {
-									name : '小飞',
-									gender : '男',
-									age : 19
-								} ]
-							},
-							methods : {
-								sayhi : function(msg) {
-									this.message = msg;
-								},
-								click : function(msg) {
-									console.log(msg);
-								}
-							},
-							created : function() {
-								console.log("Vue实例创建了");
-							},
-						});
+                            el : "#app",
+                            data : {
+                                name : "Vue啊",
+                                num : 1,
+                                message : "你们好！",
+                                word : "<h1>世界和平</h1>",
+                                dinner : [],
+                                show : true,
+                                color : "blue",
+                                showColor : true,
+                                birthday : 152468466666,
+                                watchValue : {
+                                    name : "jack",
+                                    age : 20,
+                                },
+                                peoples : [ {
+                                    name : '小亮',
+                                    gender : '男',
+                                    age : 20
+                                }, {
+                                    name : '小红',
+                                    gender : '女',
+                                    age : 18
+                                }, {
+                                    name : '小飞',
+                                    gender : '男',
+                                    age : 19
+                                } ]
+                            },
+                            methods : {
+                                sayhi : function(msg) {
+                                    this.message = msg;
+                                },
+                                click : function(msg) {
+                                    console.log(msg);
+                                }
+                            },
+                            computed : {
+                                birth : function() {
+                                    console.log("创建birth属性");
+                                    const d = new Date(this.birthday);
+                                    return d.getFullYear() + "年" + d.getMonth() + "月" + d.getDay()
+                                            + "日";
+                                }
+                            },
+                            watch : {
+                                num : function(newVal, oldVal) {
+                                    console.log(newVal + "," + oldVal);
+                                },
+                                watchValue : {
+                                    deep : true,
+                                    handler : function(newVal) {
+                                        console.log(newVal.name + "," + newVal.age);
+                                    }
+                                }
+                            },
+                            created : function() {
+                                console.log("Vue实例创建了");
+                            },
+                        });
 					</script>
 	
 	4.Vue生命周期
@@ -155,8 +182,127 @@ package vue;
 		
 		2.Vue为生命周期中的每个状态都设置了钩子函数（监听函数），Vue实例 处于不同时期时，对应的函数就会触发。(比如：在Vue中定义一个created函数)
 			beforeCreate, created, beforeMount, mounted, beforeUpdate, updated, beforeDestroy, destroyed
-			
 		
+        
+    5.按键修饰符(按键事件)
+        @keyup.enter
+              .tab
+              .delete (捕获 "删除" 和 "退格" 键)
+              .esc
+              .space
+              .up
+              .down
+              .left
+              .right
+              .ctrl
+              .alt
+              .shift
+              .meta
+
+              
+    6.组件化：
+        0.组件化：将页面的不同部分拆分成独立的组件，组合使用。
+            1.组件是Vue实例，定义时接收：data、methods、生命周期函数等。
+            2.组件无el属性，不会绑定标签。
+            3.组件有template属性，渲染需要html模板，值为HTML模板(只能有一个根标签)。
+            4.data不是对象，而是一个函数。在data函数，返回的对象中 定义数据。
+            5.组件渲染的template，只能使用组件中的data数据。
+        
+        
+        1.全局组件：
+            1.全局组件只要被加载，其他Vue对象即可使用它。
+            
+            2.定义全局组件：
+                Vue的静态方法component("",{});      --1.组件名称、2.组件参数 
+                    
+            3.定义：
+                1.<script>中定义。
+                    <script type="text/javascript">
+                        Vue.component("comp",{
+                            template : "<div><h2 style='display: inline-block;'>全局组件一：{{num}}</h2><button @click='num++'>点击计数</button></div>",
+                            data : function(){
+                                return {
+                                    num : 1,
+                                };
+                            },
+                        });
+                    </script>
+                    
+            4.使用：
+                1.html中，id作为标签。
+                2.每个组件之间数据独立。
+                    <comp></comp>
+                    
+                    
+        2.局部组件：
+            1.局部组件就是一个对象，在需要使用的Vue对象中注册，局部组件只能在当前的Vue实例中使用(当前Vue的绑定标签中使用)。
+            
+            2.定义局部组件：
+                1.<script>中定义。
+                    <script>
+                        const comp2 = {
+                            template : "<div><h2 style='display: inline-block;'>局部组件一：{{num}}</h2><button @click='num++'>点击计数</button></div>",
+                            data : function(){
+                                return {
+                                    num : 1,
+                                };
+                            },
+                        };
+                    </script>
+                    
+            3.注册并使用：
+                1.注册到Vue实例中：key组件名，value组件对象。   (EcmaScrip6语法中，key、value相同时可简写为key)
+                 
+                2.使用：组件名作为标签：
+                
+                    1.注册：
+                    <script>
+                        new Vue({
+                            el: "#app",
+                            components:{
+                                //comp2:comp2     //将定义的对象注册为组件   key组件名 value组件对象
+                                comp2,             //简写
+                            },
+                        });
+                    </script>
+                    
+                    2.使用：
+                    <body>
+                        <div id="app">
+                            <comp2></comp2>
+                        </div>
+                    </body>
+        
+        
+    7.组件间的通信：
+        1.通常一个单页应用由许多个组件组成，因此需要组件间进行通信。
+
+        
+        1.props（父向子传递）
+            1.父组件使用子组件，定义 xx属性。
+            2.子组件通过 props接收父组件 xx属性。
+            3.通过 html子组件 v-bind:指令绑定父属性，进行传递。
+                
+            子组件：
+                const comp2 = {
+                    template : "<div>{{haha}}</div>",
+                    props : ['haha'],       //props 接收父组件属性
+                };
+                
+            父组件：
+                new Vue({
+                    el : "#app",
+                    data : {
+                        message : "你们好！",
+                    }
+                    components:{
+                        comp2,             //简写
+                    },
+                });
+                
+            子组件标签：
+                <comp2 :haha="message"></comp2>
+        
  */
 
 public class Vue详解 {
